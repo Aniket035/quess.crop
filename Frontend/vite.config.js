aaf9d97fbee7211c55e7d7014866718e2e7341f7
Aniket035 <118649948+Aniket035@users.noreply.github.com>
2026-02-04 22:44:37 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/employees': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://quess-crop.onrender.com',
         changeOrigin: true,
         secure: false,
       },
